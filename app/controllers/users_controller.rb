3acc4729622dd8873125f90fdda4ffb61026ca13
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-03-04 15:47:34 +0100

    custom user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    User.create(user_params)
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
