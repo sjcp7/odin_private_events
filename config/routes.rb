fdf85afeb11f30fb65758fddd2ba4af7facdf771
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-03-07 22:03:28 +0100

    add index route to events

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create', as: 'create_login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   resources :users, only: [:new, :create, :show]
-  resources :events, only: [:new, :create, :show]
+  resources :events, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
