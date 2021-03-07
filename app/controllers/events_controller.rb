d6e64a36101ee272aef91d4dde96e88f04cfd87d
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-03-07 21:57:53 +0100

    add events controller, routes and new event form

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,19 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    current_user.events.create(event_params)
+  end
+
+  def show
+    @events = Event.all
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
+  end
+end
