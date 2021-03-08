c46dd21f182d5c2c41c783c1e5694f4201203ed2
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-03-08 22:05:22 +0100

    sort user events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+    scope :previous, -> { where("date < ?", Date.today) }
+    scope :upcoming, -> { where("date >= ?", Date.today) }
     belongs_to :creator, class_name: 'User'
     has_many :invitations
     has_many :attendees, through: :invitations
